7d2d1d1a7d5a370cae08c04ee09fd8dfc7784a8a
Jaen Coza Pedro <pedroxjaen@outlook.es>
2023-02-24 10:26:35 +0100

    Hola

---
+++ src/sesion01/Baraja.java
@@ -0,0 +1,11 @@
+package sesion01;
+
+import java.util.Random;
+
+public class Baraja {
+
+	private String nombreBaraja;
+	private Carta[] cartas;
+	
+
+}
---
+++ src/sesion01/Carta.java
@@ -0,0 +1,9 @@
+package sesion01;
+
+public class Carta implements Comparable{
+
+	private String palo;
+	private int numero;
+	private int valor;
+	
+}
