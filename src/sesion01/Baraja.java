6494c582493617c9bc392aa49a2d50ee10c2d5d4
Jaen Coza Pedro <pedroxjaen@outlook.es>
2023-03-10 10:20:56 +0100

    GTA1S0115

--- src/sesion01/Baraja.java
+++ src/sesion01/Baraja.java
@@ -4,6 +4,13 @@ import java.util.Random;
 
 public class Baraja {
 
+	public Baraja(String string, Carta[] cartas2) {
+		
+		nombreBaraja = string;
+		cartas = cartas2;
+		
+	}
+	
 	private String nombreBaraja;
 	private Carta[] cartas;
 	
--- src/sesion01/Carta.java
+++ src/sesion01/Carta.java
@@ -6,4 +6,60 @@ public class Carta implements Comparable{
 	private int numero;
 	private int valor;
 	
+	/**
+	 * Instantiates a new carta.
+	 *
+	 * @param j the j
+	 * @param i the i
+	 */
+	public Carta(String j, int i) {
+		
+		switch(j)
+		{
+			case "O":
+				valor = i + 0;
+				break;
+				
+			case "C":
+				valor = i + 12;
+				break;
+				
+			case "E":
+				valor = i + 24;
+				break;
+				
+			case "B":
+				valor = i + 36;
+				break;
+					
+			default:
+				valor = -1;
+		}	
+		
+		this.palo = j;
+		this.numero = i;
+		
+	}	
+	
+	public int getValor () {
+		return valor;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public String getPalo() {
+		return palo;
+	}
+	
+	public int compareTo(Object o) {
+		
+		Carta hola = (Carta) o;
+		
+	
+	}
+	
+	//GTA1S0115
+	
 }
