e5f392ad700d58d8d8b1bb3c49f9e400c1cc5e25
aula18 <aula18@A_18_06.aulastic2012.ual.es>
2023-04-21 10:31:15 +0200

    GTA150698

---
+++ src/sesion05/SolucionesBusqueda.java
@@ -0,0 +1,25 @@
+package sesion05;
+
+public class SolucionesBusqueda {
+
+	public static Object busquedaBinariaRec(String[] array, String clave) {
+		int primero = 0;
+		int ultimo = array.length - 1;
+		int central = array.length / 2;
+		
+		if ((clave.compareTo(array[0]) < 0) || (clave.compareTo(array[array.length - 1]) > 0))
+		{
+			return -1;
+		}
+		else
+		{
+			if(clave.compareTo(array[central]) == 0 ) {
+				
+			}
+		}
+			
+			return 3;
+	}
+	
+	//GTA150698
+}
