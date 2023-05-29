df26e16675fdd27ca31e7211ec028edb5d715967
Jaén Coza Pedro <pedroxjaen@outlook.es>
2023-05-29 11:14:07 +0200

    sesion05: fix SolucionesBusquedaTestA, Ordenacion

--- src/sesion05/SolucionesBusqueda.java
+++ src/sesion05/SolucionesBusqueda.java
@@ -1,26 +1,67 @@
 package sesion05;
 
 public class SolucionesBusqueda {
-
-	public static Object busquedaBinariaRec(String[] array, String clave) {
-		int primero = 0;
-		int ultimo = array.length - 1;
-		int central = array.length / 2;
+	
+	public static int busquedaLineal(Busqueda busqueda) throws ElementoNoEncontradoException 
+	{
+		int buscado;
 		
-		if ((clave.compareTo(array[0]) < 0) || (clave.compareTo(array[array.length - 1]) > 0))
+		buscado = busqueda.getNumBuscado();
+		
+		boolean encontrado = false;
+		int[] datos = busqueda.getDatos();
+		
+		for (int i = 0; i < datos.length; i++) 
 		{
-			return -1;
-		}
-		else
-		{
-			if(clave.compareTo(array[central]) == 0 ) {
-				
+			if (buscado == datos[i]) 
+			{
+				encontrado = true;
+				break;
 			}
 		}
-			
-			return 3;
+		
+		if (!encontrado)
+			throw new ElementoNoEncontradoException("No encontrado");
+		
+		return buscado;
 	}
-	
-	//GTA150698 
-	//test
+
+	public static int busquedaBinariaIter(Busqueda busqueda) throws ElementoNoEncontradoException 
+	{
+		int primero = 0;
+		int[] datos = busqueda.getDatos();
+		int ultimo = datos.length - 1;
+		int central;
+		int clave = busqueda.getNumBuscado();
+		
+		if (clave < datos[0] || clave > datos[datos.length - 1])
+			return -1;
+		else 
+		{
+			while (ultimo >= primero) 
+			{
+				central = (primero + ultimo) / 2;
+				
+				if (clave < datos[central])
+					ultimo = central - 1;
+				else if (clave > datos[central])
+					primero = central + 1;
+				else
+					return central;
+			}
+		}
+		
+		throw new ElementoNoEncontradoException("No encontrado");
+	}
+
+	public static int busquedaBinariaRec(Busqueda busqueda) throws ElementoNoEncontradoException 
+	{
+		int buscado = busqueda.getNumBuscado();
+				
+		return buscado;
+	}
+
+
 }
+
+// GTA150698
